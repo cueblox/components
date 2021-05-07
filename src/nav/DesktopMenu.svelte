04b17fe194fde8c33394a141fd7338acf507a7e9
Brian Ketelsen <bketelsen@gmail.com>
2021-05-07 13:36:30 -0400

    reorder colors for clarity

---
+++ src/nav/DesktopMenu.svelte
@@ -0,0 +1,105 @@
+<script>
+	import { page } from '$app/stores';
+	export let navigation;
+
+	let open = false;
+
+	function handleOpen(event) {
+		open = !open;
+	}
+</script>
+
+<div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+	<div class="flex justify-between h-16">
+		<div class="flex px-2 lg:px-0">
+			<div class="flex-shrink-0 flex inline-flex items-center px-1 pt-1z text-sm font-medium">
+				<slot id="brand"></slot>	
+			</div>
+			<div class="hidden lg:ml-6 lg:flex lg:space-x-8">
+				{#each navigation as item}
+					<a
+						href={item.route}
+						sveltekit:prefetch
+						class="{item.route === $page.path
+							? 'border-cyan-500 text-gray-900'
+							: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+					>
+						{item.name}
+					</a>
+				{/each}
+			</div>
+		</div>
+		<div class="flex-1 flex items-center justify-center px-2 lg:ml-6 lg:justify-end">
+			<div class="max-w-lg w-full lg:max-w-xs">
+				<label for="search" class="sr-only">Search</label>
+				<div class="relative">
+					<div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+						<svg
+							class="h-5 w-5 text-gray-400"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+							aria-hidden="true"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</div>
+					<input
+						id="search"
+						name="search"
+						class="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						placeholder="Search"
+						type="search"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="flex items-center lg:hidden">
+			<!-- Mobile menu button -->
+			<button
+				type="button"
+				class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+				aria-controls="mobile-menu"
+				aria-expanded="false"
+				on:click={handleOpen}
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="block h-6 w-6"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/>
+				</svg>
+				<svg
+					class="hidden h-6 w-6"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="hidden lg:ml-4 lg:flex lg:items-center" />
+	</div>
+</div>
---
+++ src/nav/NavBar.svelte
@@ -0,0 +1,4 @@
+<nav class="bg-white shadow">
+    <slot name="desktop"></slot>
+    <slot name="mobile"></slot>
+</nav>
