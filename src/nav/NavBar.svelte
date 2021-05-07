242217736f1f63bcf9f8204aff28e03b35c24951
Brian Ketelsen <bketelsen@gmail.com>
2021-05-07 14:51:07 -0400

    playing with events

--- src/nav/NavBar.svelte
+++ src/nav/NavBar.svelte
@@ -1,15 +1,3 @@
-<script>
-    	import { createEventDispatcher } from 'svelte';
-        const dispatch = createEventDispatcher();
-        
-    let menuOpen = false;
-
-    function handleToggle(event) {
-		menuOpen = !menuOpen;
-	}
-
-</script>
-
 <nav class="bg-white shadow">
     <slot name="desktop"></slot>
     <slot name="mobile"></slot>
