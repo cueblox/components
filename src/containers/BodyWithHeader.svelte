80dc2aee5801cd87526ddd7bb998a61db68f383a
Brian Ketelsen <bketelsen@gmail.com>
2021-05-07 10:40:42 -0400

    header contrast

--- src/containers/BodyWithHeader.svelte
+++ src/containers/BodyWithHeader.svelte
@@ -8,7 +8,7 @@
 	<div class="relative">
 		<div class="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
 			<h2 class="text-base font-semibold tracking-wider text-contrast-800 uppercase">{title}</h2>
-			<p class="mt-2 text-3xl font-extrabold text-main-900 tracking-tight sm:text-4xl">
+			<p class="mt-2 text-3xl font-extrabold text-contrast-900 tracking-tight sm:text-4xl">
 				{heading}
 			</p>
 			<p class="mt-5 mx-auto max-w-prose text-xl text-main-500">
