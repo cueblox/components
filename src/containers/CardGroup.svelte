40cfcd9bdd634810af56d6a4c42c4d153c853341
Brian Ketelsen <bketelsen@gmail.com>
2021-05-07 10:48:38 -0400

    Card Group

---
+++ src/containers/CardGroup.svelte
@@ -0,0 +1,5 @@
+<div
+class="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl"
+>
+<slot></slot>
+</div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { default as ArticleCard } from './cards/ArticleCard.svelte';
 import { default as BodyWithHeader } from './containers/BodyWithHeader.svelte';
+import { default as CardGroup } from './containers/CardGroup.svelte';
 import { default as PageContainer } from './containers/PageContainer.svelte';
 
 export { BodyWithHeader };
 export { PageContainer };
 export { ArticleCard };
+export { CardGroup };
