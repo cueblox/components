7e90e57c611e0d9a211b401937275f7823e6a080
Brian Ketelsen <bketelsen@gmail.com>
2021-05-08 05:57:34 -0400

    contrast color for title

--- src/headers/Simple.svelte
+++ src/headers/Simple.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div>
-	<h2 class="text-3xl tracking-tight font-extrabold text-main-900 sm:text-4xl">
+	<h2 class="text-3xl tracking-tight font-extrabold text-contrast-800 sm:text-4xl">
 		{title}
 	</h2>
 	<p class="mt-3 text-xl text-main-500 sm:mt-4">
