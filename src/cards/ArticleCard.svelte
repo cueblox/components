64bf3bb1877484baafa26e97de0fdfc5e6ea7645
Brian Ketelsen <bketelsen@gmail.com>
2021-05-08 09:48:57 -0400

    borderless variables

--- src/cards/ArticleCard.svelte
+++ src/cards/ArticleCard.svelte
@@ -1,5 +1,47 @@
 <script>
   export let article;
+
+  const colorMap = {
+	  "red": {
+		  "bg": "bg-red-100",
+		  "text": "text-red-800"
+	  },
+	  "purple": {
+		  "bg": "bg-purple-100",
+		  "text": "text-purple-800"
+	  },
+	  "blue": {
+		  "bg": "bg-blue-100",
+		  "text": "text-blue-800"
+	  },
+	  "lime": {
+		  "bg": "bg-lime-100",
+		  "text": "text-lime-800"
+	  },
+	  "amber": {
+		  "bg": "bg-amber-100",
+		  "text": "text-amber-800"
+	  },
+	  "orange": {
+		  "bg": "bg-orange-100",
+		  "text": "text-orange-800"
+	  },
+	  "teal": {
+		  "bg": "bg-teal-100",
+		  "text": "text-teal-800"
+	  },
+	  "bluegray": {
+		  "bg": "bg-bluegray-100",
+		  "text": "text-bluegray-800"
+	  },
+  }
+
+  function pillColor(color) {
+		return (
+			'inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium ' + 
+      colorMap[color]["bg"] + ' ' + colorMap[color]["text"]
+		);
+	}
 </script>
 
 <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
@@ -16,7 +58,12 @@
     <div class="flex-1">
       <p class="text-sm font-medium text-contrast-600">
         <a href="/categories/{article.category_id}" class="hover:underline">
-          {article.category && article.category.name}
+          <span
+          class="{pillColor(article.category.color)}"
+        >
+        {article.category && article.category.name}
+        </span>
+         
         </a>
       </p>
       <a href="/articles/{article.id}" class="block mt-2">
@@ -30,7 +77,7 @@
     </div>
     <div class="flex items-center mt-6">
       <div>
-        <div class="flex text-sm space-x-1 text-main-500">
+        <div class="flex space-x-1 text-sm text-main-500">
           <time datetime="2020-03-10"> Mar 10, 2020 </time>
           <span aria-hidden="true"> &middot; </span>
           <span> 4 min read </span>
