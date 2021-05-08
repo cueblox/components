424f6dc913f19c8dc497a1e6f7a478bd13ec5475
Brian Ketelsen <bketelsen@gmail.com>
2021-05-08 08:14:24 -0400

    clean up insetimage

--- src/content/InsetImage.svelte
+++ src/content/InsetImage.svelte
@@ -3,8 +3,7 @@
 	export let title;
 	export let lede;
 	export let image;
-	export let imagealt;
-  export let caption;
+
 </script>
 
 <div class="bg-white overflow-hidden">
@@ -29,14 +28,14 @@
           <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
             <figure>
               <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-                <img class="rounded-lg shadow-lg object-cover object-center" src="{image.file_name}" alt="{imagealt}" width="{image.width}" height="{image.height}">
+                <img class="rounded-lg shadow-lg object-cover object-center" src="{image.file_name}" alt="{image.alt}" width="{image.width}" height="{image.height}">
               </div>
               <figcaption class="mt-3 flex text-sm text-main-500">
                 <!-- Heroicon name: solid/camera -->
                 <svg class="flex-none w-5 h-5 text-main-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path fill-rule="evenodd" d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                 </svg>
-                <span class="ml-2">{caption}</span>
+                <span class="ml-2">{image.caption}</span>
               </figcaption>
             </figure>
           </div>
