206944796ee98112415c5c8ad89ee2192fc748dc
Brian Ketelsen <bketelsen@gmail.com>
2021-05-08 09:39:13 -0400

    borderless variables

--- src/cards/SimpleCard.svelte
+++ src/cards/SimpleCard.svelte
@@ -4,26 +4,47 @@
     export let description;
     export let link;
     
-    const keeprs = [
-        'bg-teal-600',
-		'bg-purple-600',
-		'bg-lime-600',
-		'bg-amber-600',
-		'bg-orange-600',
-		'bg-red-600',
-		'bg-bluegray-600',
-    ]
+
+    const colorMap = {
+	  "red": {
+		  "bg": "bg-red-600"
+	  },
+	  "purple": {
+		  "bg": "bg-purple-600"
+	  },
+	  "blue": {
+		  "bg": "bg-blue-600"
+	  },
+	  "lime": {
+		  "bg": "bg-lime-600"
+	  },
+	  "amber": {
+		  "bg": "bg-amber-600"
+	  },
+	  "orange": {
+		  "bg": "bg-orange-600"
+	  },
+	  "teal": {
+		  "bg": "bg-teal-600"
+
+	  },
+	  "bluegray": {
+		  "bg": "bg-bluegray-600"
+
+	  },
+  }
+
 
     function cardColor(color) {
 		return (
-			'flex-shrink-0 flex items-center justify-center w-16 bg-' +
-			color +
-			'-600 text-white text-sm font-medium rounded-l-md'
+			'flex-shrink-0 flex items-center justify-center w-16 ' +
+			colorMap[color]["bg"] +
+			' text-white text-sm font-medium rounded-l-md'
 		);
 	}
 </script>
 
-<li class="flex col-span-1 shadow-sm rounded-md">
+<li class="flex col-span-1 rounded-md shadow-sm">
     <div class={cardColor(color)}>
         {title.charAt(0)}
     </div>
