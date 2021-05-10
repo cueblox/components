f4ebda094605a1920f7887442879477e9e643768
David McKay <david@rawkode.com>
2021-05-10 12:30:09 +0100

    feat: add sociallink story

---
+++ src/social/SocialLink.stories.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import SocialLink from './SocialLink.svelte';
+</script>
+
+<Meta
+	title="Social/SocialLink"
+	component={SocialLink}
+	argTypes={{
+		network: { control: 'text' },
+		username: { control: 'text' },
+		url: { control: 'text' }
+	}}
+/>
+
+<Template let:args>
+	<SocialLink {...args} />
+</Template>
+
+<Story
+	name="Twitter"
+	args={{
+		network: 'twitter',
+		username: 'rawkode',
+		url: 'https://twitter.com/rawkode'
+	}}
+/>
+
+<Story
+	name="GitHub"
+	args={{
+		network: 'github',
+		username: 'rawkode',
+		url: 'https://github.com/rawkode'
+	}}
+/>
