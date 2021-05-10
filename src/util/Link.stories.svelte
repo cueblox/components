ddc527ee780e6eeca579df971cc6341cb949c855
David McKay <david@rawkode.com>
2021-05-10 12:40:06 +0100

    feat: add link story

---
+++ src/util/Link.stories.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Link from './Link.svelte';
+</script>
+
+<Meta
+	title="Util/Link"
+	component={Link}
+	argTypes={{
+		href: { control: 'text' },
+		label: { control: 'text' },
+		blank: { control: 'bool' }
+	}}
+/>
+
+<Template let:args>
+	<Link {...args}>Some Link</Link>
+</Template>
+
+<Story
+	name="Google with Blank"
+	args={{
+		href: 'https://google.com',
+		label: 'Google',
+		blank: true
+	}}
+/>
+
+<Story
+	name="Google without Blank"
+	args={{
+		href: 'https://google.com',
+		label: 'Google',
+		blank: false
+	}}
+/>
+
+<Story
+	name="Inpage Link"
+	args={{
+		href: 'Stuff',
+		label: 'Stuff in Page',
+		blank: false
+	}}
+/>
